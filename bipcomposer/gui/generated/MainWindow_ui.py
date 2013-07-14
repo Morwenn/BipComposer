9e81dc58c554c5cdcc4c5b3f3eae4b38df905d02
Morwenn <morwenn29@hotmail.fr>
2013-07-14 03:59:17 +0200

    Menus: shortcuts for Alt and smart use of ellipsis.

--- bipcomposer/gui/generated/MainWindow_ui.py
+++ bipcomposer/gui/generated/MainWindow_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'bipcomposer\gui\generated\MainWindow.ui'
 #
-# Created: Sat Jul 13 11:47:53 2013
+# Created: Sun Jul 14 03:54:01 2013
 #      by: pyside-uic 0.2.14 running on PySide 1.1.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -88,14 +88,14 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_('MainWindow'))
-        self.menuFile.setTitle(_('File'))
-        self.menuEdit.setTitle(_('Edit'))
+        self.menuFile.setTitle(_('&File'))
+        self.menuEdit.setTitle(_('&Edit'))
         self.toolBar.setWindowTitle(_('toolBar'))
-        self.actionNew.setText(_('New'))
-        self.actionOpen.setText(_('Open'))
-        self.actionSave.setText(_('Save'))
-        self.actionSaveAs.setText(_('Save as'))
-        self.actionCancel.setText(_('Undo'))
-        self.actionRestore.setText(_('Redo'))
+        self.actionNew.setText(_('&New'))
+        self.actionOpen.setText(_('&Open...'))
+        self.actionSave.setText(_('&Save'))
+        self.actionSaveAs.setText(_('Save &as...'))
+        self.actionCancel.setText(_('&Undo'))
+        self.actionRestore.setText(_('&Redo'))
 
 import bipcomposer.icons
