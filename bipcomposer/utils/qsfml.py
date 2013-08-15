39624ff4ac04071503d411b028242e3b08416bfe
Morwenn <morwenn29@hotmail.fr>
2013-08-15 14:45:40 +0200

    Fixed a bug with self.view.

--- bipcomposer/utils/qsfml.py
+++ bipcomposer/utils/qsfml.py
@@ -38,21 +38,21 @@ class QSFMLCanvas(QWidget):
         self.initialized = False
 
         self.window = sf.HandledWindow()
+        # Have the canvas act like its underlying RenderTarget
         self.__dict__['draw'] = self.window.draw
         self.__dict__['clear'] = self.window.clear
-        self.__dict__['view'] = self.window.view
         self.__dict__['display'] = self.window.display
 
-        # setup some states to allow direct rendering into the widget
+        # Setup some states to allow direct rendering into the widget
         self.setAttribute(Qt.WA_PaintOnScreen)
         self.setAttribute(Qt.WA_OpaquePaintEvent)
         self.setAttribute(Qt.WA_NoSystemBackground)
         self.setAttribute(Qt.WA_StaticContents)
 
-        # set strong focus to enable keyboard events to be received
+        # Set strong focus to enable keyboard events to be received
         self.setFocusPolicy(Qt.StrongFocus)
 
-        # setup the timer
+        # Setup the timer
         self.timer = QTimer()
         self.timer.setInterval(frameTime)
 
@@ -70,7 +70,7 @@ class QSFMLCanvas(QWidget):
 
     def showEvent(self, event):
         if not self.initialized:
-            # under X11, we need to flush the commands sent to the server
+            # Under X11, we need to flush the commands sent to the server
             # to ensure that SFML will get an updated view of the windows
             # create the SFML window with the widget handle
             if platform.system() == 'Linux':
@@ -85,21 +85,26 @@ class QSFMLCanvas(QWidget):
         hwnd = ctypes.pythonapi.PyCapsule_GetPointer(self.winId(), None)
         self.window.create(hwnd)
 
-        # let the derived class do its specific stuff
+        # Let the derived class do its specific stuff
         self.onInit()
 
-        # setup the timer to trigger a refresh at specified framerate
+        # Setup the timer to trigger a refresh at specified framerate
         self.timer.timeout.connect(self.repaint)
         self.timer.start()
 
         self.initialized = True
 
     def paintEvent(self, event):
-        # let the derived class do its specific stuff
+        # Let the derived class do its specific stuff
         self.onUpdate()
-        # display on screen
+        # Display on screen
         self.display()
 
-
+    @property
+    def view(self):
+        return self.window.view
+    @view.setter
+    def view(self, value):
+        self.window.view = value
 
 
